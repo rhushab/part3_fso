51c5bb8b548331cf77b2615b86d6b70b9b64399c
rhushab <rhushabhbontapallee@googlemail.com>
2023-03-07 08:40:31 +0530

    added static build

--- phonebook/index.js
+++ phonebook/index.js
@@ -98,6 +98,6 @@ app.post(`/api/persons`, (request, response) => {
   console.log(morgan.token);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT);
 console.log(`Server running on ${PORT}`);
